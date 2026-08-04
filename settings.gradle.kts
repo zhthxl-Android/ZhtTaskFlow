7ccdab3726135704d25de5360a80954bfd965637
zht <304938584@qq.com>
2026-08-04 10:59:07 +0800

    添加注释

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,6 +14,7 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
+    // 开启仓库统一管控
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         // 阿里云公共依赖镜像
