611429a323e92a07f2d317ecbd5b4183ec323550
zht <304938584@qq.com>
2026-08-06 08:35:17 +0800

    公共底层组件分支

--- app/src/main/kotlin/com/example/zhttaskflow/MainActivity.kt
+++ app/src/main/kotlin/com/example/zhttaskflow/MainActivity.kt
@@ -9,6 +9,7 @@ import com.example.zhttaskflow.nav.theme.TaskFlowTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //公共底层组件分支 创建
         setContent {
             TaskFlowTheme {
             }
