55994d203637092723fb99c932f8a40ced62aa83
zht <304938584@qq.com>
2026-09-02 18:01:15 +0800

    路由门禁与深链自动化测试

--- component_nav/build.gradle.kts
+++ component_nav/build.gradle.kts
@@ -5,3 +5,15 @@ plugins {
 taskFlow {
     resourcePrefix.set("nav_")
 }
+
+android {
+    testOptions {
+        unitTests.isIncludeAndroidResources = true
+    }
+}
+
+dependencies {
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.robolectric)
+}
